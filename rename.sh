5f023b4752a51b108559c58722262bc07200f50e
Szyroi <117603175+Szyroi@users.noreply.github.com>
2026-08-23 16:35:04 +0200

    Added: Wallpapers & Rename script

---
+++ rename.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+path="$HOME/Pictures/P/WP"
+
+mkdir -p -v "$path" && cd "$path" || exit
+
+i=1
+
+for file in *; do
+    if [ -f "$file" ] && [ "$file" != "rename.sh" ]; then
+
+        if [[ "$file" =~ ^[0-9]+$ ]]; then
+            continue
+        fi
+
+        while [ -e "${i}" ]; do
+            ((i++))
+        done
+
+        new_name="${i}"
+        mv -- "$file" "$new_name"
+        echo "Renamed: $file -> $new_name"
+
+        ((i++))
+    fi
+done
+
+echo "DONE!!!"
